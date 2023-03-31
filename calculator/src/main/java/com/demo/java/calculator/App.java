bfaf8cb8fd6c7fb027685f9ac83cace2111b713a
prism200ssb <prism200ssb@gmail.com>
2023-03-31 17:10:53 +0530

    This commit consist of addition functionallity and test cases

--- calculator/src/main/java/com/demo/java/calculator/App.java
+++ calculator/src/main/java/com/demo/java/calculator/App.java
@@ -1,13 +1,29 @@
 package com.demo.java.calculator;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+
+public class App {
+	public static int Addition(int num1, int num2) throws Exception {
+		if(num1<= 0 || num2<=0 ) 
+			throw new Exception("Invalid numbers");
+		else {
+			int result =0;
+			result = num1 + num2;
+			return result;
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("Addition of given number is " +Addition(10,20));
+		
+	}
+	
+}    
--- calculator/src/test/java/com/demo/java/calculator/AppTest.java
+++ calculator/src/test/java/com/demo/java/calculator/AppTest.java
@@ -3,18 +3,25 @@ package com.demo.java.calculator;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.testng.Assert;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+{@Test
+	public void positiveAdditionTest() throws Exception {
+	System.out.println("Test case 1 passed...");
+	
+}
+
+@Test
+public void negativeAdditionTest() {
+	System.out.println("Test case 2 passes...");
+}
+@Test
+public void positiveAdditionTest2()  {
+	System.out.println("Test case 3 passed");
+	
+}
 }
